e8bd1e3ac48fb1f0ea505ee4c8b4f8f7a9187d69
camilo andres lemos <camilolemus30@gmail.com>
2023-10-29 11:06:20 -0500

    vistas y rutas del adminAlmacen

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/indexAdmin', function () {
+    return view('indexAdmin');
+})->name('indexAdmin');
+
+Route::get('/layout', function () {
+    return view('layout');
+})->name('layout');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/password', function () {
+    return view('password');
+})->name('password');
+
+Route::get('/registro', function () {
+    return view('registro');
+})->name('registro');
+
+
+
+Route::get('/inventario', function () {
+    return view('inventario');
+})->name('inventario');
