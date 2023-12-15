505532587fab2091239f959e69c10e28dcac4e49
camilo andres lemos <camilolemus30@gmail.com>
2023-12-15 18:46:07 -0500

    ultimo final

--- public/js/consumoTiendas.js
+++ public/js/consumoTiendas.js
@@ -21,12 +21,16 @@ function leerEmpresas() {
                     const imagenesArray = JSON.parse(element.logo);
 
                     const textCarta = `<div class="card mt-5 mb-5 " style="max-width: 25rem; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-          <img src="${imagenesArray[0]}" class="card-img-top" alt="..." style="object-fit: cover; border-radius: 15px 15px 0 0; height: 200px; width: 100%; object-position: center center;">
+          <img src="${
+              imagenesArray != null ? imagenesArray[0] : ""
+          }" class="card-img-top" alt="..." style="object-fit: cover; border-radius: 15px 15px 0 0; height: 200px; width: 100%; object-position: center center;">
           <div class="card-body text-white bg-dark">
               <h5 class="card-title">${element.nombre}</h5>
               <p class="card-text">${element.nit}</p>
               <p class="card-text">${element.descripcion}</p>
-              <a href="/pagina-principal" class="btn btn-dark" onclick="seleccionarEmpresa(${element.id})" style="background-color: rgb(245, 90, 0);">Ver tienda</a>
+              <a href="/pagina-principal" class="btn btn-dark" onclick="seleccionarEmpresa(${
+                  element.id
+              })" style="background-color: rgb(245, 90, 0);">Ver tienda</a>
           </div>
       </div>
       `;
